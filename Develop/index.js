b23674e92ad9e42470436afb8d0c76179b6dc174
Cory Piette <coryjpiette@gmail.com>
2021-07-24 15:43:56 -0400

    adding consts

---
+++ Develop/index.js
@@ -0,0 +1,14 @@
+
+
+// creating variable and requiring Markdown.js 
+var generateMarkdown = require("./develop/Markdown");
+
+// creating variable and function to pull fs
+const fs = require("fs");
+
+// creating variable and function  to pull inquirer
+const inquirer = require("inquirer");
+
+// creating variable and using require to use npm's path//doesn't need install//part of npm
+const path = require("path");
+
---
+++ Develop/markdown.js
@@ -0,0 +1,16 @@
+// function to create markdown for README
+function generateMarkdown(data) {
+    // returns inquirer data
+    return `# ${data.title}
+  ----
+  <a href="https://img.shields.io/badge/License-${data.license[0]}-brightgreen"><img src="https://img.shields.io/badge/License-${data.license[0]}-brightgreen"></a>
+
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Testing](#testing)
+  - [Contact Information](#contact-information)
+  
+}
