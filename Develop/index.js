e43d4dca4c6e977eb948b6ac8a008b17b510d76c
Cory Piette <coryjpiette@gmail.com>
2021-07-24 16:22:56 -0400

    adding function to write README files

--- Develop/index.js
+++ Develop/index.js
@@ -9,6 +9,14 @@ const fs = require("fs");
 // creating variable and function  to pull inquirer
 const inquirer = require("inquirer");
 
-// creating variable and using require to use npm's path//doesn't need install//part of npm
+
+// creating variable and using require to use npm
 const path = require("path");
 
+
+// function to write README file takes in fileName and user data parameters
+function writeToFile(fileName, data) {
+    // writing and syncing file
+    fs.writeFileSync(path.join(process.cwd(), fileName), data);
+  }
+  
--- Develop/markdown.js
+++ Develop/markdown.js
@@ -2,7 +2,7 @@
 function generateMarkdown(data) {
     // returns inquirer data
     return `# ${data.title}
-  ----
+ 
   <a href="https://img.shields.io/badge/License-${data.license[0]}-brightgreen"><img src="https://img.shields.io/badge/License-${data.license[0]}-brightgreen"></a>
 
   ## Table of Contents
@@ -13,4 +13,27 @@ function generateMarkdown(data) {
   - [Testing](#testing)
   - [Contact Information](#contact-information)
   
-}
+
+
+### Description
+${data.description}
+
+### Installation
+${data.installation}
+
+### Usage
+${data.usage}
+### Contribution
+
+${data.contribution}
+### Test-Instructions
+
+${data.tests}
+
+### Contact-Information
+[Github Profile](https://github.com/${data.username})
+${data.email}
+
+
+';
+} 
