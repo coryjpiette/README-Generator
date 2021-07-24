e86e5f68d440230d35e1c4b8dd78af038e0bb8ba
Cory Piette <coryjpiette@gmail.com>
2021-07-24 16:41:34 -0400

    adding imput functions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,7 @@
 
 
 // creating variable and requiring Markdown.js 
-var generateMarkdown = require("./develop/Markdown");
+var generateMarkdown = require("./markdown");
 
 // creating variable and function to pull fs
 const fs = require("fs");
@@ -18,5 +18,61 @@ const path = require("path");
 function writeToFile(fileName, data) {
     // writing and syncing file
     fs.writeFileSync(path.join(process.cwd(), fileName), data);
-  }
-  
+}
+// function to initialize program
+
+function initialize() {
+    // prompted questions from the inquirer
+
+    inquirer.prompt([
+        {
+
+            type: "input",
+            name: "username",
+            message: "Please enter Your Github username"
+        },
+        {
+
+            type: "input",
+            name: "email",
+            message: "Please enter a valid email address"
+        },
+        {
+
+            type: "input",
+            name: "title",
+            message: "Please enter your project's name"
+        },
+        {
+
+            type: "input",
+            name: "description",
+            message: "Please enter a project descrition"
+        },
+        {
+
+            type: "input",
+            name: "installation",
+            message: "Please enter your installation instructions"
+        },
+        {
+
+            type: "input",
+            name: "usage",
+            message: "Enter the appropiate usage Info"
+        },
+        {
+
+            type: "input",
+            name: "contribution",
+            message: "Please enter the contribution guidelines"
+        },
+        {
+
+            type: "input",
+            name: "tests",
+            message: "Please enter the test instructions"
+        }
+    ]
+    )
+}
